b1a310655f26437be4b4146cd1a662f657a3da1f
preston-mcmann <102309425+preston-mcmann@users.noreply.github.com>
2022-04-18 10:21:25 -0400

    Add final version ofBanking App

---
+++ Online Banking/src/online/banking/CheckingAccount.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package online.banking;
+
+/**
+ *
+ * @author mcman
+ */
+public class CheckingAccount extends Account {
+        double interestRate=0.03;
+        public CheckingAccount( double balance) {
+            super( balance); 
+            
+	}
+        
+}
