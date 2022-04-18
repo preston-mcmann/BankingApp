04bfb420b7d9afbd696744c7e83d573a86d4063d
preston-mcmann <102309425+preston-mcmann@users.noreply.github.com>
2022-04-18 12:06:51 -0400

    Final Banking app

---
+++ Online Banking/src/online/banking/Branch.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package online.banking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mcman
+ */
+public class Branch {
+
+    String name;
+    private List<User> userList;
+    List<String> userNames = new ArrayList<>();
+    List<String> userPassword = new ArrayList<>();
+    List<String> userSSN = new ArrayList<>();
+
+    public Branch(String name, List<User> userList) {
+        this.name = name;
+        this.userList = userList;
+    }
+
+    public List<User> getUsers() {
+        return userList;
+    }
+
+    public void print() {
+        for (User u : userList) {
+            System.out.print(u.getName());
+        }
+    }
+
+
+
+    public int compare(String Username, String SSN, String Password) {
+        int index = -1;
+        for (int i = 0; i < userList.size(); i++) {
+             if(userList.get(i).getName().equals(Username) && userList.get(i).getPassword().equals(Password)&&userList.get(i).getSsn().equals(SSN)){
+                index = i;
+             }
+        }       
+        return index;
+        }
+    
+    public void addUser(String Username, String SSN, String Password, double checking, double saving){
+        User user = new User(Username,SSN,Password,checking, saving);
+        userList.add(user);
+    }
+    public void removeUser(User user){
+        userList.remove(user);
+    }
+    //getters
+    public String getName() {
+        return this.name;
+    }
+
+    //setters
+    public void setName(String Name) {
+        this.name = Name;
+    }
+}
